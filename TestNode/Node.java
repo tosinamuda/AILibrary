9cec5df4839ea832c081b093c57f3be968580d7b
Tosin Amuda <tosinamuda@gmail.com>
2014-11-08 00:01:39 +0100

    Add New File

--- TestNode/Node.java
+++ TestNode/Node.java
@@ -14,7 +14,7 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 
-public class Node
+public class Node implements Comparable<Object>
 {
     private Point _drawingLocation;
     private Point _center;
@@ -27,6 +27,13 @@ public class Node
     public Node(String label, double diameter, Point location)
     {
         this._drawingLocation = location;
+        this._label = label;  
+        this._diameter = diameter;
+    }
+    
+     public Node(String label, double diameter)
+    {
+      
         this._label = label;  
         this._diameter = diameter;
     }
@@ -50,7 +57,30 @@ public class Node
     }
     public Point Center()
     {
-        return this._center;
+        //return this._center;
+        //center equals (x+radius,y+radius) where x and y is the upper left corner of the rectangle for a circle
+        return new Point((int)(this._drawingLocation.getX() + this._diameter / 2), (int)(this._drawingLocation.getY() + this._diameter / 2));
     }
+    
+     
+    public int compareTo(Object o) throws ArithmeticException {
+        Node node = (Node)o;
         
+        if (Integer.parseInt(this._label) > Integer.parseInt(node.Label()))
+            return 1;
+        else if (Integer.parseInt(this._label) < Integer.parseInt(node.Label()))
+            return -1;
+        else 
+            return 0;
+        }
+       
+     public void drawCircle(Graphics g)
+     {
+         int diameter = (int)this._diameter;
+        g.fillOval((int)this._drawingLocation.getX(), (int)this._drawingLocation.getY(), diameter, diameter);
+     }   
+    
+     protected void paintComponent(Graphics g) {
+         drawCircle(g);
+    }   
 }
