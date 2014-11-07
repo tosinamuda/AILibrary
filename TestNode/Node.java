a09a679c7ff73360b9c8e81af5cfb086c9f25ca7
Tosin Amuda <tosinamuda@gmail.com>
2014-11-07 21:17:42 +0100

    initial project version

---
+++ TestNode/Node.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TestNode;
+
+/**
+ *
+ * @author BaldEagle
+ */
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+public class Node
+{
+    private Point _drawingLocation;
+    private Point _center;
+    private String _label;
+    private final double _diameter;
+    
+    public Node(String label, double diameter, Point location)
+    {
+        this._drawingLocation = location;
+        this._label = label;  
+        this._diameter = diameter;
+    }
+    
+    public Node()
+    {
+        
+    }    
+}
